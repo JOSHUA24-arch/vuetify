3437f777e385934d1d5a9a1c3108e11ece41f7b7
JOSHUA24-arch <mallyajoshua24@gmail.com>
2022-08-15 02:16:06 +0300

    this is vuetify

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about pa-6">
+    <h1>About our company</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, sunt quas doloremque voluptatum vel distinct
+    io fugit eaque quibusdam officiis soluta libero molestiae sed repudiandae facere recusandae deleniti? Fugit, ipsum maxime.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, sunt quas doloremque voluptatum vel distinct
+    io fugit eaque quibusdam officiis soluta libero molestiae sed repudiandae facere recusandae deleniti? Fugit, ipsum maxime.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, sunt quas doloremque voluptatum vel distinct
+    io fugit eaque quibusdam officiis soluta libero molestiae sed repudiandae facere recusandae deleniti? Fugit, ipsum maxime.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, sunt quas doloremque voluptatum vel distinct
+    io fugit eaque quibusdam officiis soluta libero molestiae sed repudiandae facere recusandae deleniti? Fugit, ipsum maxime.</p>
+  </div>
+</template>
